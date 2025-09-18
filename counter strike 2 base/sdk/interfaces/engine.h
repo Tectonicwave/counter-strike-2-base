158ea20e01cd247a5efccdf789e155648650c639
Tectonicwave <Damiensawyer18@gmail.com>
2025-09-18 16:42:15 +1000

    update to true index

--- counter strike 2 base/sdk/interfaces/engine.h
+++ counter strike 2 base/sdk/interfaces/engine.h
@@ -3,47 +3,47 @@
 
 namespace sdk
 {
-    class INetChannelInfo
-    {
-    public:
+	class INetChannelInfo
+	{
+	public:
 
-        float get_network_latency()
-        {
-            return utils::CallVFunc<float, 12U>(this);
-        }
+		float get_network_latency()
+		{
+			return utils::CallVFunc<float, 10U>(this);
+		}
 
-        float get_engine_latency()
-        {
-            return utils::CallVFunc<float, 13U>(this);
-        }
-    };
+		float get_engine_latency()
+		{
+			return utils::CallVFunc<float, 11U>(this);
+		}
+	};
 
-    struct cengine_client
-    {
-        bool in_game()
-        {
-            return utils::CallVFunc<bool, 36U>(this);
-        }
+	struct cengine_client
+	{
+		bool in_game()
+		{
+			return utils::CallVFunc<bool, 38U>(this);
+		}
 
-        bool is_connected()
-        {
-            return utils::CallVFunc<bool, 37U>(this);
-        }
+		bool is_connected()
+		{
+			return utils::CallVFunc<bool, 39U>(this);
+		}
 
-        INetChannelInfo* get_net_channel_info(int nSplitScreenSlot = 0)
-        {
-            return utils::CallVFunc<INetChannelInfo*, 39U>(this, nSplitScreenSlot);
-        }
+		INetChannelInfo* get_net_channel_info(int nSplitScreenSlot = 0)
+		{
+			return utils::CallVFunc<INetChannelInfo*, 39U>(this, nSplitScreenSlot);
+		}
 
-        int get_local_player()
-        {
-            int nIndex = -1;
+		int get_local_player()
+		{
+			int nIndex = -1;
 
-            utils::CallVFunc<void, 49U>(this, std::ref(nIndex), 0);
+			utils::CallVFunc<void, 53U>(this, std::ref(nIndex), 0);//52
 
-            return nIndex + 1;
-        }
+			return nIndex + 1;
+		}
 
-    };
+	};
 
 } // namespace sdk
